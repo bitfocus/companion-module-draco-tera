6722ee2f0f401939224511cefe5f036453797e45
Steve Tinsley <steve@stcomputing.net>
2019-05-05 21:11:21 +0100

    initial code commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# draco-tera-kvm
+
+
---
+++ index.js
@@ -0,0 +1,187 @@
+var tcp = require('../../tcp');
+var instance_skel = require('../../instance_skel');
+var debug;
+var log;
+
+function instance(system, id, config) {
+		var self = this;
+
+		// super-constructor
+		instance_skel.apply(this, arguments);
+		self.status(1,'Instance Initializing');
+		self.actions(); // export actions
+		return self;
+}
+
+
+
+
+instance.prototype.init = function() {
+	var self = this;
+	debug = self.debug;
+	log = self.log;
+	self.init_tcp();
+};
+
+
+instance.prototype.updateConfig = function(config) {
+	var self = this;
+	self.config = config;
+	self.init_tcp();
+};
+
+
+
+
+
+instance.prototype.init_tcp = function() {
+	var self = this;
+
+	if (self.socket !== undefined) {
+		self.socket.destroy();
+		delete self.socket;
+	}
+
+	if (self.config.host) {
+		self.socket = new tcp(self.config.host, 5555);
+
+		self.socket.on('status_change', function (status, message) {
+			self.status(status, message);
+		});
+
+		self.socket.on('error', function (err) {
+			debug("Network error", err);
+			self.status(self.STATE_ERROR, err);
+			self.log('error',"Network error: " + err.message);
+		});
+
+		self.socket.on('connect', function () {
+			self.status(self.STATE_OK);
+			debug("Connected");
+		})
+
+		self.socket.on('data', function (data) {});
+	}
+};
+
+
+
+
+
+
+// Fields for web config
+instance.prototype.config_fields = function () {
+		var self = this;
+		return [
+				{
+						type: 'text',
+						id: 'info',
+						width: 12,
+						label: 'Information',
+						value: 'This module is for Draco Tera KVM'
+				},
+				{
+						type: 'textinput',
+						id: 'host',
+						label: 'Target IP',
+						width: 12,
+						default: '192.168.100.99',
+						regex: self.REGEX_IP
+				}
+		];
+};
+
+// When module gets deleted
+instance.prototype.destroy = function () {
+		var self = this;
+
+		if (self.socket !== undefined) {
+				self.socket.destroy();
+		}
+		debug("destroy", self.id);
+};
+
+instance.prototype.actions = function (system) {
+		var self = this;
+
+		var actions = {
+
+				'setconnection': {
+						label: 'Set Connection CPU > CON',
+						options: [{
+								type: 'textinput',
+								label: 'CPU',
+								id: 'cpu',
+								default: '',
+								tooltip: 'Enter CPU Number',
+								regex: self.REGEX_NUMBER
+						},{
+								type: 'textinput',
+								label: 'CON',
+								id: 'con',
+								default: '',
+								tooltip: 'Enter CON Number',
+								regex: self.REGEX_NUMBER
+						}]
+				}
+		};
+
+		self.setActions(actions);
+};
+
+
+
+
+
+
+instance.prototype.action = function (action) {
+		// console.log("GO");
+		var self = this;
+		var id = action.action;
+		var opt = action.options;
+		var cmd;
+
+		function getHexPart(d, /*short integer*/ p  /*part 0 or 1*/) {
+			var cp = Number(d).toString(16);
+			cp = "0000".substring(0,4 - cp.length) + cp;
+			// debug(cp.substring(p*2,(p+1)*2))
+			return cp.substring(p*2,(p+1)*2)
+		}
+
+		switch (id) {
+
+			case 'setconnection':
+				//cmd = '0x1B 0x5B 0x49 0x09 0x00 0xC9 0x0B 0xF4 0x03';
+
+				// cmd = Buffer.from([
+				// 	0x1B,
+				// 	0x5B,
+				// 	0x49,
+				// 	0x09,
+				// 	0x00,
+				// 	parseInt(opt.cpu),
+				// 	parseInt(opt.con)
+				// ]);
+
+				cmd = new Buffer( "1B5B490900"+getHexPart(opt.con,1)+getHexPart(opt.con,0)+getHexPart(opt.cpu,1)+getHexPart(opt.cpu,0), "hex")
+			break;
+
+		}
+
+		if (cmd !== undefined) {
+				if (self.socket !== undefined) {
+						debug('sending ', cmd, "to", self.socket.host);
+						self.socket.send(cmd);
+				}
+		}
+};
+
+
+
+
+
+
+
+
+instance_skel.extendedBy(instance);
+exports = module.exports = instance;
